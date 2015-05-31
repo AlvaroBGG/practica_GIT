a1488570bf75bc78d1522bf762b8257f7f95b762
AlvaroEXAMEN <Alvaro@Alvaro>
2015-05-31 20:14:27 +0200

    En la clase "Principal" hemos creado tres instancias de cada clase:Producto, Cliente, Vendedor y Tienda.

--- Tienda_Alvaro_015/src/tienda/Principal.java
+++ Tienda_Alvaro_015/src/tienda/Principal.java
@@ -1,5 +1,24 @@
 package tienda;
 
 public class Principal {
-
+	
+	public static void main(String[] args){
+	
+	Clientes cliente1 = new Clientes();
+	Clientes cliente2 = new Clientes();
+	Clientes cliente3 = new Clientes();
+	
+	Vendedores vendedor1 = new Vendedores();
+	Vendedores vendedor2 = new Vendedores();
+	Vendedores vendedor3 = new Vendedores();
+	
+	Productos producto1 = new Productos();
+	Productos producto2 = new Productos();
+	Productos producto3 = new Productos();
+	
+	Tiendas tienda1 = new Tiendas();
+	Tiendas tienda2 = new Tiendas();
+	Tiendas tienda3 = new Tiendas();	
+	}
+	
 }
